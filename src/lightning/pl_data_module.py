b488f2dcfef20303269294d5429a4cc7ec549a32
shubharajkharel <shubha.raj.kharel@gmail.com>
2023-09-20 17:56:10 -0400

    stratified pl datamodule

--- src/lightning/pl_data_module.py
+++ src/lightning/pl_data_module.py
@@ -1,54 +1,124 @@
-from typing import Optional
-from torch.utils.data import random_split
-import torch
-from torch.utils.data import Dataset, DataLoader
+import pickle
+import os
+from typing import Literal, Optional
 
-# import lightning.pytorch as pl
+import numpy as np
 import pytorch_lightning as pl
+import torch
+from sklearn.model_selection import StratifiedShuffleSplit
+from torch.utils.data import DataLoader, Dataset, Subset
 
 
 class PlDataModule(pl.LightningDataModule):
     def __init__(
         self,
         dataset: Dataset,
-        seed: int = 42,
         train_size: float = 0.7,
         val_size: float = 0.15,
+        random_seed: float = 42,
         batch_size: int = 128,
+        num_workers: int = 10,
+        pin_memory: bool = True,
+        shuffle: bool = True,
+        persistent_workers: bool = True,
+        stratify: Optional[bool] = False,
+        stratification_labels: Optional[torch.Tensor] = None,
+        **data_loader_kwargs: dict,  # useful for stratified sampling
     ):
         super().__init__()
         self.dataset = dataset
-        self.seed = seed
-
+        self.random_seed = random_seed
         # this is required from batch_size tuner
         # https://lightning.ai/docs/pytorch/stable/advanced/training_tricks.html
         self.batch_size = batch_size
-
         self.train_size = train_size
         self.val_size = val_size
+        self.stratify = stratify
+        self.stratification_labels = stratification_labels
 
-    def setup(self, stage: Optional[str] = None):
+        self.data_loader_kwargs = {
+            **data_loader_kwargs,
+            "batch_size": batch_size,
+            "num_workers": num_workers,  # number of subprocesses
+            "pin_memory": pin_memory,  # faster transfer to GPU
+            "shuffle": shuffle,  # reshuffle at every epoch
+            "persistent_workers": persistent_workers,  # keep workers alive
+        }
+
+        self.train_idx, self.val_idx, self.test_idx = (
+            self._create_stratified_idx() if self.stratify else self._create_idx()
+        )
+
+    def setup(self, stage: Optional[Literal["fit", "test", "predict"]] = None):
+        if stage == "fit":
+            self.train_data = Subset(self.dataset, self.train_idx)
+            self.val_data = Subset(self.dataset, self.val_idx)
+        elif stage == "test":
+            self.test_data = Subset(self.dataset, self.test_idx)
+        elif stage == "predict":
+            self.predict_data = Subset(self.dataset, self.test_idx)
+
+    def train_dataloader(self):
+        return DataLoader(self.train_data, **self.data_loader_kwargs)
+
+    def val_dataloader(self):
+        return DataLoader(self.val_data, **self.data_loader_kwargs)
+
+    def test_dataloader(self):
+        return DataLoader(self.test_data, **self.data_loader_kwargs)
+
+    def predict_dataloader(self):
+        return DataLoader(self.predict_data, **self.data_loader_kwargs)
+
+    def _create_idx(self):
         # stage option is not used here
         # but is passed in by tuner
         train_len = int(self.train_size * len(self.dataset))
         val_len = int(self.val_size * len(self.dataset))
         test_len = len(self.dataset) - train_len - val_len
         assert test_len >= 0, ValueError("train_size + val_size must be less than 1")
-        self.train_data, self.val_data, self.test_data = random_split(
-            self.dataset,
-            [train_len, val_len, test_len],
-            generator=torch.Generator().manual_seed(self.seed),
+        idx = np.arange(len(self.dataset))
+        if self.random_seed is not None:
+            np.random.seed(self.random_seed)
+            np.random.shuffle(idx)
+        train_idx = idx[:train_len]
+        val_idx = idx[train_len : train_len + val_len]
+        test_idx = idx[train_len + val_len :]
+
+        return train_idx, val_idx, test_idx
+
+    def _create_stratified_idx(self):
+        # TODO: very slow currently, using pickle for cache
+        cache_file = "stratified_idx.pkl"
+        if os.path.exists(cache_file):
+            with open(cache_file, "rb") as f:
+                return pickle.load(f)
+
+        if self.stratification_labels is None:
+            raise ValueError(
+                "stratification_labels must be provided when stratify is True"
+            )
+        train_idx, temp_idx = next(
+            StratifiedShuffleSplit(
+                n_splits=1,
+                train_size=self.train_size,
+                random_state=self.random_seed,
+            ).split(self.dataset, self.stratification_labels)
         )
-        self.predict_data = self.test_data
+        temp_labels = self.stratification_labels[temp_idx]
+        val_idx, test_idx = next(
+            StratifiedShuffleSplit(
+                n_splits=1,
+                train_size=self.val_size / (1 - self.train_size),
+                random_state=self.random_seed,
+            ).split(
+                np.zeros(len(temp_idx)), temp_labels
+            )  # zeros are dummy
+        )
 
-    def train_dataloader(self):
-        return DataLoader(self.train_data, batch_size=self.batch_size)
+        # Cache the output
+        indices = train_idx, val_idx, test_idx
+        with open(cache_file, "wb") as f:
+            pickle.dump(indices, f)
 
-    def val_dataloader(self):
-        return DataLoader(self.val_data, batch_size=self.batch_size)
-
-    def test_dataloader(self):
-        return DataLoader(self.test_data, batch_size=self.batch_size)
-
-    def predict_dataloader(self):
-        return DataLoader(self.predict_data, batch_size=self.batch_size)
+        return train_idx, val_idx, test_idx
--- tests/tests_lightning/test_pl_datamodule.py
+++ tests/tests_lightning/test_pl_datamodule.py
@@ -1,5 +1,5 @@
 import unittest
-
+from torch.utils.data import DataLoader
 import torch
 from torch.utils.data import Dataset
 
@@ -7,9 +7,10 @@ from src.lightning.pl_data_module import PlDataModule
 
 
 class SimpleDataset(Dataset):
-    def __init__(self, length=1000):
+    def __init__(self, length=10000):
         self.data = torch.randn(length, 10)
         self.targets = torch.randint(0, 2, (length,))
+        self.labels = self.targets
 
     def __getitem__(self, index):
         return self.data[index], self.targets[index]
@@ -25,6 +26,8 @@ class TestData(unittest.TestCase):
         cls.dataset = SimpleDataset()
         cls.data_module = PlDataModule(cls.dataset)
         cls.data_module.setup()
+        cls.data_module.setup(stage="fit")
+        cls.data_module.setup(stage="test")
 
     def test_dtype(self):
         self.assertEqual(type(self.dataset[0][0]), torch.Tensor)
@@ -45,6 +48,37 @@ class TestData(unittest.TestCase):
 
         data, _ = next(iter(self.data_module.test_dataloader()))
         self.assertEqual(len(data), self.data_module.batch_size)
+
+    def test_stratification(self):
+        stratified_data_module = PlDataModule(
+            self.dataset,
+            stratify=True,
+            stratification_labels=self.dataset.targets,
+        )
+        stratified_data_module.setup(stage="fit")
+        train_targets = torch.cat(
+            [y for _, y in DataLoader(stratified_data_module.train_data)]
+        )
+        val_targets = torch.cat(
+            [y for _, y in DataLoader(stratified_data_module.val_data)]
+        )
+        stratified_data_module.setup(stage="test")
+        test_targets = torch.cat(
+            [y for _, y in DataLoader(stratified_data_module.test_data)]
+        )
+
+        total_targets = len(self.dataset.targets)
+
+        train_ratio = train_targets.sum().item() / len(train_targets)
+        val_ratio = val_targets.sum().item() / len(val_targets)
+        test_ratio = test_targets.sum().item() / len(test_targets)
+        original_ratio = self.dataset.targets.sum().item() / total_targets
+
+        # Assuming a tolerance of 2%
+        threshold = 0.02
+        self.assertAlmostEqual(train_ratio, original_ratio, delta=threshold)
+        self.assertAlmostEqual(val_ratio, original_ratio, delta=threshold)
+        self.assertAlmostEqual(test_ratio, original_ratio, delta=threshold)
 
 
 if __name__ == "__main__":
