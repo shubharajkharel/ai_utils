5a1b4f07ef17531ddaf23831b64ef9acba34835d
shubharajkharel <shubha.raj.kharel@gmail.com>
2023-10-18 17:28:43 -0400

    misc

--- src/lightning/loggers/tb/log_weights_hist.py
+++ src/lightning/loggers/tb/log_weights_hist.py
@@ -1,27 +1,33 @@
 # import pytorch_lightning as pl
+
 import lightning as pl
 import torch
 from pytorch_lightning import Callback
-
-
-import logging
 
 
 class TensorboardLogAllWeigthsHist(Callback):
     """Logs histogram of weight at end of training"""
 
     def on_train_end(self, trainer, pl_module):
-        from utils.src.misc.model_adapters import PLAdapter
+        if isinstance(
+            pl_module.logger, pl.pytorch.loggers.tensorboard.TensorBoardLogger
+        ):
+            # comment out to avoid unnecessary dependencies
+            from utils.src.misc.model_adapters import PLAdapter
 
-        if isinstance(pl_module.logger, pl.loggers.tensorboard.TensorBoardLogger):
             weight_list = PLAdapter(pl_module).get_weights().values()
             weights = [weight.flatten() for weight in weight_list]
+
+            # weights = []
+            # for _, param in pl_module.named_parameters():
+            #     weights.append(param.flatten())
+
             pl_module.logger.experiment.add_histogram(
                 tag="weights",
                 values=torch.cat(weights),
                 global_step=pl_module.current_epoch,
             )
         else:
-            logging.getLogger().warn(
+            raise NotImplementedError(
                 "Logging weight histogram for non-Tensorboard Logger not implemented"
             )
